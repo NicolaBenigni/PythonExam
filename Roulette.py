0cfd92fd53e131aa2c0e90d59556ae078c32b2b9
NicolaBenigni <nicola.benigni@outlook.com>
2017-01-25 19:46:47 +0100

    Preliminary steps of the exam (slide 9)

--- Roulette.py
+++ Roulette.py
@@ -1,10 +1,11 @@
-print("Welcome to this evaluation exam, fasten you seat belt and be ready to take off")
-
-# Import library called random
+# Import libraries called intertools and random
+import itertools
 import random
+
 # Fix a seed, say 3456, so that even random draws can be replicated
 random.seed(3456)
-# Test that the seed works correctly: expected outcome: 7, 8, 5, 3, 9
-print(random.randint(1,10))
-print(random.randint(1,10))
+
+# Test that the seed works correctly: expected random outcome: 7, 8, 5, 3, 9
+five_random_integers = [random.randint(1, 10) for _ in itertools.repeat(None, 5)]
+print(five_random_integers)
 
