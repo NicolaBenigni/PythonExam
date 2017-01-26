6a02aab3997f95a8d3fe8b89c2387d7862c243b9
NicolaBenigni <nicola.benigni@outlook.com>
2017-01-26 12:09:15 +0100

    Created new file Simulation.py

--- Roulette.py
+++ Roulette.py
@@ -5,7 +5,7 @@ import random
 # Fix a seed, say 3456, so that even random draws can be replicated
 random.seed(3456)
 
-# Test that the seed works correctly: expected random outcome: 7, 8, 5, 3, 9
+# Test whether the seed works correctly: expected random outcome: 7, 8, 5, 3, 9
 five_random_integers = [random.randint(1, 10) for _ in itertools.repeat(None, 5)]
 print(five_random_integers)
 
