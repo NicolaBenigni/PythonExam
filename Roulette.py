b3e747f19effc4b977be9fdafe82b05022913b61
NicolaBenigni <nicola.benigni@outlook.com>
2017-01-27 11:33:07 +0100

    Tried to implement roulette game

--- Roulette.py
+++ Roulette.py
@@ -9,3 +9,43 @@ random.seed(3456)
 five_random_integers = [random.randint(1, 10) for _ in itertools.repeat(None, 5)]
 print(five_random_integers)
 
+
+# Create a roulette game
+
+class Table(object):
+    def above_min(self, amount):
+        return self
+
+    def spin_the_wheel(self, bet):
+        return self
+
+    def simulate_game(self, bet, amount):
+        return self
+
+
+class Roulette(Table):
+    def __init__(self, min_bet):
+        self.min_bet = min_bet
+
+    def above_min(self, amount):
+        if amount >= self.min_bet:
+            return 1
+        else:
+            return 0
+
+    def spin_the_wheel(self, bet):
+        print("spinning the wheel...")
+        print("the ball lands on %s." % random.randint(0, 36))
+        if bet == random.randint(0, 36):
+            return "the bet is correct"
+        else:
+            return "the bet is incorrect"
+
+    def simulate_game(self, bet, amount):
+        if bet == random.randint(0, 36) and self.above_min == 1:
+            return [0, amount*30]
+        else:
+            return [amount, 0]
+
+
+
--- Simulation.py
+++ Simulation.py
@@ -1,1 +1,4 @@
-print("Hi world")
+amount1 = 101
+bet1 = 24
+table1 = Roulette.Roulette(100)
+print(table1.simulate_game(bet1, amount1))
